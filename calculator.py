893d520f3dfc216b96ec65482b78474c601d1679
joseph_oderoh <oderohk@gmail.com>
2026-04-21 23:02:46 +0300

    use of inbuilt functions

--- calculator.py
+++ calculator.py
@@ -1,29 +1,33 @@
-num1 = float(input("Enter the first number: "))
-num2= float(input("Enter the second number: "))
+# num1 = float(input("Enter the first number: "))
+# num2= float(input("Enter the second number: "))
 
-print("1: Add")
-print("2: Subtraction")
-print("3: Multiply")
-print("4: Divide")
+# print("1: Add")
+# print("2: Subtraction")
+# print("3: Multiply")
+# print("4: Divide")
 
 
-operation = input("Enter the number coresponding to the operation: ")
-if operation == "1":
-    result = num1 + num2
-    print(f"{num1} + {num2} = {result}")
-elif operation == "2":
-    result = num1 - num2
-    print(f"{num1} - {num2} = {result}")
-elif    operation == "3":
-    result = num1 * num2
-    print(f"{num1} * {num2} = {result}")
-elif operation == "4":
-    if num2 != 0:
-        result = num1 / num2
-        print(f"{num1} / {num2} = {result}")
-    else:
-        print("Erro :Divixon by zero")
-else:
-    print("Invalid inpu")            
+# operation = input("Enter the number coresponding to the operation: ")
+# if operation == "1":
+#     result = num1 + num2
+#     print(f"{num1} + {num2} = {result}")
+# elif operation == "2":
+#     result = num1 - num2
+#     print(f"{num1} - {num2} = {result}")
+# elif    operation == "3":
+#     result = num1 * num2
+#     print(f"{num1} * {num2} = {result}")
+# elif operation == "4":
+#     if num2 != 0:
+#         result = num1 / num2
+#         print(f"{num1} / {num2} = {result}")
+#     else:
+#         print("Erro :Divixon by zero")
+# else:
+#     print("Invalid inpu")            
    
-  
+x = int(input("What's x?"))
+y = int(input("What's y?"))
+
+# z = int(x) + int(y)
+print(x+y)
